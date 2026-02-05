55f8d7fc39e555e067a21852b08304ab9aeef5a2
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2026-02-05 17:33:46 -0300

    [IsAnagram] - thinking about the solution final

--- src/main/kotlin/LeetCode/IsAnagram/isAnagram.kt
+++ src/main/kotlin/LeetCode/IsAnagram/isAnagram.kt
@@ -4,17 +4,13 @@ package LeetCode.IsAnagram
 // c, a, r, r, c, e
 
 fun isAnagram(s: String, t: String): Boolean {
-    val Scount = mutableMapOf<Char, Int>()
+    if(s.length != t.length) return false
 
-    s.forEach {
-        Scount[it] = Scount.getOrDefault(it, 0) + 1
-        if (Scount[it] == s.length) return true
-    }
-    return false
+    return s.toCharArray().sorted() == t.toCharArray().sorted()
 }
 
 fun main() {
     val s = "ccar"
-    val t = "ttar"
+    val t = "carc"
     println(isAnagram(s = s, t = t))
 }
