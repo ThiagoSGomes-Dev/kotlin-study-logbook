4e2e978f4811e51d2b8c456d1ef7fd8e5a5ab85c
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2026-02-05 09:22:20 -0300

    [IsAnagram] - thinking about the solution 0/1

--- src/main/kotlin/LeetCode/IsAnagram/isAnagram.kt
+++ src/main/kotlin/LeetCode/IsAnagram/isAnagram.kt
@@ -1,12 +1,15 @@
 package LeetCode.IsAnagram
 
+// Posso -> comparar o tamanho de cada string
+// c, a, r, r, c, e
+
 fun isAnagram(s: String, t: String): Boolean {
     s.contains(t) // true
     return false
 }
 
 fun main() {
-    val s = "racecar"
-    val t = "carrace"
+    val s = "ccar"
+    val t = "ttar"
     println(isAnagram(s = s, t = t))
 }
