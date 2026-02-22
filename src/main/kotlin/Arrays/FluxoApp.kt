da992c9b0adab57a1f6ef0360de36a88bc688950
ThiagoSGomes-Dev <thiagodos.10@gmail.com>
2026-02-21 21:58:55 -0300

    [feat] - Converte essa String para um enum usando entries

--- src/main/kotlin/Arrays/FluxoApp.kt
+++ src/main/kotlin/Arrays/FluxoApp.kt
@@ -22,6 +22,16 @@ enum class HttpMethod(val label: String) {
     GET("GET"),
     POST("POST"),
     PUT("PUT");
+
+    companion object {
+        private val map = entries.associateBy { it.label.lowercase() }
+
+        fun fromLabel(value: String): HttpMethod {
+            return map[value.lowercase()]
+                ?: throw IllegalArgumentException("Método inválido: $value")
+        }
+    }
+
 }
 
 
